0dd2342926913bd26d6ad5500a88f558829e7a5e
Jitanjali Patel <jitanjalipatel19@gmail.com>
2025-08-01 16:17:29 +0530

    🐛 Fixed model path loading issue for Streamlit

--- predictivesystem.py
+++ predictivesystem.py
@@ -7,10 +7,16 @@ This is a temporary script file.
 
 import numpy as np
 import pickle
+import os
+
+model_path = os.path.join(os.path.dirname(__file__), 'trained_model.pkl')
+with open(model_path, 'rb') as f:
+    loaded_model = pickle.load(f)
+
 
 #we give path where train_model.sav file exist by forward slash it shoul contain .sav file
 # loading the saved model
-loaded_model = pickle.load(open('trained_model.pkl', 'rb'))
+#loaded_model = pickle.load(open('trained_model.pkl', 'rb'))
 
 input_data = (5,166,72,19,175,25.8,0.587,51)
 
